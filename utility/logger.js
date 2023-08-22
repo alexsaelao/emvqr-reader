ffcb281ec1d4c161a8e34fc2b5dd851fd3223a4f
DESKTOP-82UI805\info <alexcodesabai@gmail.com>
2023-08-22 15:22:32 +0700

    edit log file

--- utility/logger.js
+++ utility/logger.js
@@ -1,18 +1,18 @@
 const path = require('path');
 const fs = require('fs')
-const FileStreamRotator = require('file-stream-rotator')
+// const FileStreamRotator = require('file-stream-rotator')
 
-exports.logDir = () => {
-    const logDirectory = path.join(__dirname, '../logs')
-    fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
-    const accessLogStream = FileStreamRotator.getStream({
-        date_format: 'YYYYMMDD',
-        filename: path.join(logDirectory, 'access-%DATE%.log'),
-        interval: '1d',
-        verbose: false,
-    });
-    return accessLogStream
-}
+// exports.logDir = () => {
+//     const logDirectory = path.join(__dirname, '../logs')
+//     fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+//     const accessLogStream = FileStreamRotator.getStream({
+//         date_format: 'YYYYMMDD',
+//         filename: path.join(logDirectory, 'access-%DATE%.log'),
+//         interval: '1d',
+//         verbose: false,
+//     });
+//     return accessLogStream
+// }
 
 exports.logging = function (req, content) {
     const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
